c00c58b6afd1fee473870e66a0f0651a6e870aef
arthdarji352 <arthdarji20@gmail.com>
2024-01-07 14:23:33 +0530

    userSchema add

---
+++ models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+//schema
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "user name is required"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, "email is required"],
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    address: {
+      type: Array,
+    },
+    phone: {
+      type: String,
+      required: [true, "phone is required"],
+    },
+    userType: {
+      type: String,
+      required: [true, "user type is required"],
+      default: "client",
+      enum: ["client", "admin", "vendor", "driver"],
+    },
+    profile: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);

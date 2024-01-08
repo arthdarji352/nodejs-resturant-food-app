9d4c1074e453668d29a1c990386f282a467edc72
arthdarji352 <arthdarji20@gmail.com>
2024-01-08 12:59:02 +0530

    add password hashing and JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
 
 // REGISTER
 const registerController = async (req, res) => {
@@ -20,12 +22,15 @@ const registerController = async (req, res) => {
         message: "email Already Registerd please Login",
       });
     }
+    //password hashing
+    var salt = bcrypt.genSaltSync(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
     //create new user
     const user = await userModel.create({
       userName,
       email,
-      password,
+      password: hashedPassword,
       address,
       phone,
     });
@@ -56,7 +61,7 @@ const loginController = async (req, res) => {
     }
 
     //check user
-    const user = await userModel.findOne({ email: email, password: password });
+    const user = await userModel.findOne({ email });
     if (!user) {
       return res.status(404).send({
         success: false,
@@ -64,9 +69,26 @@ const loginController = async (req, res) => {
       });
     }
 
+    //check user password | compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(500).send({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    //toker
+    const token = JWT.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    user.password = undefined;
+
     res.status(200).send({
       success: true,
       message: "Login successfuly",
+      token,
       user,
     });
   } catch (error) {
